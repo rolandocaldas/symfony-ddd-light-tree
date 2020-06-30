81c3295f25f850cef243a12560ceb346779ad1f3
Rolando Caldas <rolando.caldas@gmail.com>
2020-06-30 04:25:26 +0200

    Implemented Example repository

---
+++ application/src/Infrastructure/Persistence/Doctrine/Repository/DoctrineExampleRepository.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Infrastructure\Persistence\Doctrine\Repository;
+
+use App\Domain\Example\Example;
+use App\Domain\Example\ExampleRepository;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+class DoctrineExampleRepository extends ServiceEntityRepository implements ExampleRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Example::class);
+    }
+
+    public function save(Example $example): void
+    {
+        return;
+    }
+
+}
